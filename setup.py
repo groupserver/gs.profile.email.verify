13f819d467b8bf8bed76c7afa1e13c255917ab64
Alice Murphy <alice@onlinegroups.net>
2010-12-10 18:57:58 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,40 @@
+# coding=utf-8
+import os
+from setuptools import setup, find_packages
+from version import get_version
+
+version = get_version()
+
+setup(name='gs.profile.email.verify',
+    version=version,
+    description="Verifying an email address on GroupServer.",
+    long_description=open("README.txt").read() + "\n" +
+                      open(os.path.join("docs", "HISTORY.txt")).read(),
+    classifiers=[
+      "Development Status :: 4 - Beta",
+      "Environment :: Web Environment",
+      "Framework :: Zope2",
+      "Intended Audience :: Developers",
+      "License :: Other/Proprietary License",
+      "Natural Language :: English",
+      "Operating System :: POSIX :: Linux"
+      "Programming Language :: Python",
+      "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+    keywords='profile email address verify verification groupserver',
+    author='Alice Murphy',
+    author_email='alice@onlinegroups.net',
+    url='http://groupserver.org/',
+    license='other',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['gs', 'gs.profile', 'gs.profile.email'],
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[
+        'setuptools',
+        # -*- Extra requirements: -*-
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+    """,)
+
