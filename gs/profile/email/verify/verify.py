7d09a1ce72a1e2bf690a185772dd56d6a601a6d9
Alice Murphy <alice@onlinegroups.net>
2010-12-30 15:34:31 +0100

    Content provider for the JS

--- gs/profile/email/verify/verify.py
+++ gs/profile/email/verify/verify.py
@@ -2,16 +2,30 @@
 from zope.component import createObject
 from Products.Five import BrowserView
 from Products.CustomUserFolder.interfaces import IGSUserInfo
+from queries import VerificationQuery
 
 class VerifyEmailPage(BrowserView):
     def __init__(self, context, request):
         BrowserView.__init__(self, context, request)
         self.label = u'Verify Email'
         self.userInfo = IGSUserInfo(self.context)
-        self.__siteInfo = None
         self.verificationId = request.form.get('verificationId','')
-        self.email = 'admin-foo@groupsense.net'
-        
+        self.__siteInfo = self.__query = self.__email = None
+
+    @property
+    def email(self):
+        if self.__email == None:
+            self.__email = \
+              self.query.get_email_from_verificationId(self.verificationId)
+        return self.__email
+    
+    @property
+    def query(self):
+        if self.__query == None:
+            self.__query = \
+              VerificationQuery(self.context.zsqlalchemy)
+        return self.__query
+    
     @property
     def siteInfo(self):
         if self.__siteInfo == None:
