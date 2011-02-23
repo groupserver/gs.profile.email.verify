d046253b3e02dada37e8bf1d24017fb1f5e69c6f
Alice Murphy <alice@onlinegroups.net>
2011-02-23 17:07:08 +0100

    Handle special chars

--- gs/profile/email/verify/viewaddressverified.py
+++ gs/profile/email/verify/viewaddressverified.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+from urllib import unquote
 from zope.component import createObject
 from Products.Five import BrowserView
 
@@ -15,7 +16,7 @@ class AddressVerifiedView(BrowserView):
         
         assert hasattr(self.request, 'form'), 'No form in request'
         assert 'email' in self.request.form.keys(), 'No email in form'
-        email = self.request.form['email']
+        email = unquote(self.request.form['email'])
         eu = createObject('groupserver.EmailUserFromEmailAddress', 
                           self.context, email)
         verified = eu.is_address_verified(email)
