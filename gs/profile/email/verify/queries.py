765ae3e8b104c8f904ea89f3e1b6f6e43cc40ba4
mpj17@onlinegroups.net <mpj17@onlinegroups.net>
2012-06-22 16:54:47 +1200

    Switching to gs.database

--- gs/profile/email/verify/queries.py
+++ gs/profile/email/verify/queries.py
@@ -1,59 +1,73 @@
 # coding=utf-8
 import pytz, datetime
 import sqlalchemy as sa
+from gs.database import getTable, getSession
+from zope.sqlalchemy import mark_changed
 
 class EmailQuery(object):
-    def __init__(self, da, email):
-        assert da, 'No da'
-        assert email, 'No email'
+    def __init__(self, email):
         self.email = email
-        self.userEmailTable = da.createTable('user_email')
-        self.emailVerifyTable = da.createTable('email_verification')
-        # One day we will have constraints
-        aclUsers = da.site_root().acl_users
-        user = aclUsers.get_userByEmail(email)
-        assert user, 'No user for email address %s' % email
+        self.userEmailTable = getTable('user_email')
+        self.emailVerifyTable = getTable('email_verification')
         
     def set_verification_id(self, verificationId):
         assert verificationId, 'No verificationId'
         evt = self.emailVerifyTable
         i = evt.insert()
-        i.execute(verification_id = verificationId, 
-                  email = self.email)
+        d = {'verification_id': verificationId, 
+             'email': self.email}
+
+        session = getSession()
+        session.execute(i, params=d)
+        mark_changed(session)
 
     def verify_address(self, verificationId):
         assert verificationId, 'No verificationId'
         uet = self.userEmailTable
-        d = datetime.datetime.utcnow().replace(tzinfo=pytz.utc)
-        s = uet.update(sa.func.lower(uet.c.email) == self.email.lower())
-        s.execute(verified_date = d)
+        u = uet.update(sa.func.lower(uet.c.email) == self.email.lower())
+        d = {'verified_date': 
+             datetime.datetime.utcnow().replace(tzinfo=pytz.utc)}
+
+        session = getSession()
+        session.execute(u, params=d)
+        mark_changed(session)
 
     def clear_verification_ids(self):
         evt = self.emailVerifyTable
         u = evt.update(sa.and_(evt.c.email == self.email,
                                evt.c.verified == None))
-        d = datetime.datetime.utcnow().replace(tzinfo=pytz.utc)
-        u.execute(verified = d)
+        d = {'verified_date': 
+             datetime.datetime.utcnow().replace(tzinfo=pytz.utc)}
+
+        session = getSession()
+        session.execute(u, params=d)
+        mark_changed(session)
 
     def unverify_address(self):
         uet = self.userEmailTable
-        s = uet.update(sa.func.lower(uet.c.email) == self.email.lower())
-        s.execute(verified_date = None)
+        u = uet.update(sa.func.lower(uet.c.email) == self.email.lower())
+        d = {'verified_date': None}
+
+        session = getSession()
+        session.execute(u, params=d)
+        mark_changed(session)
 
 class VerificationQuery(object):
     NOT_FOUND  = 0
     CURRENT    = 1
     VERIFIED   = 2
     
-    def __init__(self, da):
-        self.userEmailTable = da.createTable('user_email')
-        self.emailVerifyTable = da.createTable('email_verification')
+    def __init__(self):
+        self.userEmailTable = getTable('user_email')
+        self.emailVerifyTable = getTable('email_verification')
         
     def get_email_from_verificationId(self, verificationId):
         evt = self.emailVerifyTable
-        s = evt.select()
+        s = sa.select([evt.c.email], limit=1)
         s.append_whereclause(evt.c.verification_id == verificationId)
-        r = s.execute().fetchone()
+
+        session = getSession()
+        r = session.execute(s).fetchone()
         retval = r and r['email'] or ''
         assert type(retval) == str
         return retval
@@ -63,9 +77,11 @@ class VerificationQuery(object):
         email = self.get_email_from_verificationId(verificationId)
         if email:
             uet = self.userEmailTable
-            s = uet.select()
+            s = sa.select([uet.c.user_id], limit=1)
             s.append_whereclause(sa.func.lower(uet.c.email) == email.lower())
-            r = s.execute().fetchone()
+
+            session = getSession()
+            r = session.execute(s).fetchone()
             retval = r and r['user_id'] or ''
             assert retval, 'No userId in user_email table for '\
               'address %s in email_verification table' % email
@@ -74,10 +90,11 @@ class VerificationQuery(object):
 
     def verificationId_status(self, verificationId):
         evt = self.emailVerifyTable
-        s = evt.select()
+        s = sa.select([evt.c.verified], limit=1)
         s.append_whereclause(evt.c.verification_id == verificationId)
         
-        r  = s.execute().fetchone()
+        session = getSession()
+        r  = session.execute(s).fetchone()
         if r:
             if r['verified'] == None:
                 retval = self.CURRENT
