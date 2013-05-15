34071630952bab3c953b40b8cbc065c6dbdb7639
Michael JasonSmith <mpj17@onlinegroups.net>
2013-05-15 15:49:09 -0700

    Updating the Check Verified JavaScript, making it work with deferred jQuery.

--- gs/profile/email/verify/browser/javascript/verify_address.js
+++ gs/profile/email/verify/browser/javascript/verify_address.js
@@ -1,89 +1,97 @@
 // GroupServer module for checking if email addresses are verified
 jQuery.noConflict();
-GSVerifyEmailAddress = function () {
+
+function GSVerifyEmailAddress(email, verificationId, statusId, siteName) {
 
     // Private variables
-    var email = null;
-    var quotedEmail = null;
-    var verificationId = null;
-    var statusUpdate = null;
-    var siteName = null;
-    var unverifiedMsg = null;
-    var verifiedMsg = null; 
-    var verifyingMsg = null;
-    var checkingMsg = null;
-    
-    var VERIFY_ADDRESS = 'verifyemail.ajax';
-    var CHECK_ADDRESS = 'checkemailverified.ajax';
-    var TIMEOUT_DELTA = 2000;
+    var unverifiedMsg = null,
+        verifiedMsg = null, 
+        verifyingMsg = null,
+        checkingMsg = null,
+        statusUpdate = null,
+        VERIFY_ADDRESS = 'verifyemail.ajax',
+        CHECK_ADDRESS = 'checkemailverified.ajax',
+        TIMEOUT_DELTA = 4000;
+
     // Private methods
+    function init() {
+        var e = null;
+        e = '<code class="email">' + email + '</code>';
+        unverifiedMsg = 'The email address ' + e + ' is ' +
+            '<strong>not verified</strong>.';
+        verifiedMsg = '<p>The email address ' + e + ' has been ' + 
+            '<strong>verified</strong>. You can use this address to send ' + 
+            'messages to your groups, receive messages from groups, and '+
+            'sign in to ' + siteName + '.</p>'+
+            '<p><strong class="label">Important:</strong> You should now ' +
+            '<strong>close</strong> this page before returning to ' +
+            siteName + '.</p>';
+        verifyingMsg = '<strong>Verifying</strong> ' + e + 
+            '&#160;<img src="/++resource++anim/wait.gif"/>';
+        checkingMsg = '<strong>Checking</strong> ' + e + 
+            '&#160;<img src="/++resource++anim/wait.gif"/>';
 
-    // Public methods and properties. The "checkServer" and "checkReturn"
-    // methods have to be public, due to oddities with "setTimeout".
-    return {
-        init: function (e, q, v, s, n) {
-            /* Add the address-checking code to the correct widgets
-            
-            ARGUMENTS
-              e:  String containing the email address
-              q:  String containing the email address, quoted with urllib
-              v:  String containing the verification id
-              s:  String containing the selector for the status-update
-                  container.
-              n:  String containing the site name
-            */
-            email = e;
-            quotedEmail = q;
-            verificationId = v;
-            statusUpdate = s;
-            siteName = n;
-            unverifiedMsg = 'The email address <code class="email">' + e + 
-              '</code> is <strong>not verified</strong>.';
-            verifiedMsg = '<p>The email address <code class="email">' + e + 
-              '</code> has been <strong>verified</strong>. You can ' + 
-              'use this address to send messages to your groups, ' +
-              'receive messages from groups, and log in to ' + n + '.</p>'+
-              '<p><strong>IMPORTANT</strong>: You should now <strong>close</strong> this page before '+
-              'returning to '+siteName+'.</p>';
-            verifyingMsg = '<strong>Verifying</strong> ' + 
-              '<code class="email">' + e + '</code>' + 
-              '&#160;<img src="/++resource++anim/wait.gif"/>';
-            checkingMsg = '<strong>Checking</strong> ' + 
-              '<code class="email">' + e + '</code>' + 
-              '&#160;<img src="/++resource++anim/wait.gif"/>';
-            jQuery(statusUpdate).html(checkingMsg);
-            setTimeout("GSVerifyEmailAddress.checkServer()", TIMEOUT_DELTA);
-        },
-        checkServer: function () {
-            jQuery.ajax({
-              type: "POST",
-              url: CHECK_ADDRESS, 
-              cache: false,
-              data: 'email='+quotedEmail,
-              success: GSVerifyEmailAddress.checkReturn});
-            jQuery(statusUpdate).html(checkingMsg);
-        },
-        checkReturn: function (data, textStatus) {
-            var verified = data == '1';
-            if (verified) {
-                jQuery(statusUpdate).html(verifiedMsg);
-            } else {
-                jQuery(statusUpdate).html(verifyingMsg);
-                setTimeout("GSVerifyEmailAddress.verifyAddress()",
-                  TIMEOUT_DELTA);
-            }
-        },
-        verifyAddress: function () {
-            jQuery.ajax({
-              type: "POST",
-              url: VERIFY_ADDRESS, 
-              cache: false,
-              data: 'verificationId='+verificationId,
-              success: GSVerifyEmailAddress.checkServer});
-        },
-        changeCheckingMessage: function() {
-            jQuery(statusUpdate).html(verifyingMsg);              
+        statusUpdate = jQuery(statusId);
+        statusUpdate.html(checkingMsg);
+    }
+    init(); // Note the automatic execution
+
+    function checkServer() {
+        var d = {type: "POST",
+                 url: CHECK_ADDRESS, 
+                 cache: false,
+                 data: 'email='+email,
+                 success: checkReturn
+                }
+        jQuery.ajax(d);
+        jQuery(statusUpdate).html(checkingMsg);
+    }
+
+    function checkReturn(data, textStatus) {
+        var verified = null;
+        verified = data == '1';
+        if ( verified ) {
+            jQuery(statusUpdate).html(verifiedMsg);
+        } else {
+            jQuery(statusUpdate).html(verifyingMsg);
+            window.setTimeout(verifyAddress, TIMEOUT_DELTA);
         }
-    };
-}(); // GSVerifyEmailAddress
+    }
+    
+    function verifyAddress() {
+        var d = {
+            type: "POST",
+            url: VERIFY_ADDRESS, 
+            cache: false,
+            data: 'verificationId='+verificationId,
+            success: checkServer
+        }
+        jQuery.ajax(d);
+    }
+    
+    function changeCheckingMessage() {
+        jQuery(statusUpdate).html(verifyingMsg);              
+    }
 
+    // Public methods and properties.
+    return {
+        start: function () {checkServer()}
+    };
+} // GSVerifyEmailAddress
+
+jQuery(window).load(function () {
+    var script = null, 
+        verificationId = null, 
+        statusId = null, 
+        siteName = null, 
+        email = null, 
+        checker = null;
+
+    script = jQuery('#gs-profile-email-verify-js');
+    email = script.attr('data-email');
+    verificationId = script.attr('data-verificationId');
+    statusId = script.attr('data-statusId');
+    siteName = script.attr('data-siteName');
+    checker = GSVerifyEmailAddress(email, verificationId, statusId, siteName);
+    checker.start();
+});
