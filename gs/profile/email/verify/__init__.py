d9ccc2aae00528b2e948d29fc59b470a4e9f5ace
Michael JasonSmith <mpj17@onlinegroups.net>
2014-01-23 11:12:54 -0800

    Adding the EmailVerificationUser to the general API

--- gs/profile/email/verify/__init__.py
+++ gs/profile/email/verify/__init__.py
@@ -1,24 +1,13 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-# Copyright © 2013 OnlineGroups.net and Contributors.
-# All Rights Reserved.
-#
-# This software is subject to the provisions of the Zope Public License,
-# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
-# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
-# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
-# FOR A PARTICULAR PURPOSE.
-#
-##############################################################################
+from __future__ import absolute_import
 from AccessControl import ModuleSecurityInfo
 from AccessControl import allow_class
-from gs.profile.email.verify.emailverificationuser import \
+#lint:disable
+from .emailverificationuser import EmailVerificationUser, \
     EmailVerificationUserFromUser
+#lint:enable
+
 allow_class(EmailVerificationUserFromUser)
-
-
 moduleId = 'gs.profile.email.verify.emailverificationuser'
 evu_security = ModuleSecurityInfo(moduleId)
 evu_security.declarePublic('EmailVerificationUser')
