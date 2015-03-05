5f6eca2e0a4c6799441f208f703cd02e9482daaa
Michael JasonSmith <mpj17@onlinegroups.net>
2015-03-05 13:06:23 -0800

    Switching from asserts to raising ValueErrors

--- gs/profile/email/verify/update.py
+++ gs/profile/email/verify/update.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-##############################################################################
+############################################################################
 #
-# Copyright © 2013 OnlineGroups.net and Contributors.
+# Copyright © 2013, 2015 OnlineGroups.net and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
@@ -11,7 +11,7 @@
 # WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
 # FOR A PARTICULAR PURPOSE.
 #
-##############################################################################
+############################################################################
 from __future__ import absolute_import
 from zope.component import createObject
 from Products.Five import BrowserView  # TODO: gs.profile.base.ProfilePage
@@ -28,13 +28,13 @@ class VerifyEmail(BrowserView):
     def __call__(self):
         assert self.request
         assert self.context
-        # FIXME: Raise an error
-        assert hasattr(self.request, 'form'), 'No form in request'
-        assert 'verificationId' in self.request.form.keys(), \
-            'No verificationId in form'
+        if not hasattr(self.request, 'form'):
+            raise ValueError('No form in request')
+        if 'verificationId' not in self.request.form.keys():
+            raise ValueError('No verificationId in form')
         vId = self.request.form['verificationId']
         evu = createObject('groupserver.EmailVerificationUserFromId',
-                            self.context, vId)
+                           self.context, vId)
         veu = VerifyEmailUser(evu.userInfo)
         if veu.verificationId_current(vId):
             evu.verify_email(vId)
